8aa5bae9741af9acb234e0658013cca1ada9e155
Millan Bhandari <millan.ib@icloud.com>
2022-08-08 15:57:52 -0500

    i forgot to commit
    functionality created for the flashcards

--- RandomNumCalc/ContentView.swift
+++ RandomNumCalc/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var num1 = 0
+    @State private var num2 = 0
+    @State private var finalCalc = 0
+    @State private var text = 0
+    @State private var text1 = ""
+    @State private var X = ""
+    @State var buttonTapped = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Number Flash Cards")
+                .font(.title)
+                .fontWeight(.bold)
+            Text("Tap the numbers to see answer")
+                .font(.footnote)
+            Spacer()
+            Text( "\(text)" + " \(X)" + " \(text1)").cornerRadius(10)
+                .frame(width: 120, height: 120, alignment: .center)
+                .onTapGesture {
+                    text = num1 * num2
+                    X = ""
+                    text1 = ""
+                }
+                .background(.green)
+            Button("New") {
+                randomCalc()
+                reset()
+            }
+            Spacer()
+            .onAppear {
+                randomCalc()
+                reset()
+            }
+        }
+    }
+    func reset() {
+        text = num1
+        X = "X"
+        text1 = String(num2)
+    }
+    func randomCalc() {
+        num1 = Int.random(in: 1...20)
+        num2 = Int.random(in: 1...20)
     }
 }
 
