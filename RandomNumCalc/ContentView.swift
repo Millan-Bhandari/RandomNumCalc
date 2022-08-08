306c18b12fe5d4691d293f0225c9a3d237b006f5
Millan Bhandari <millan.ib@icloud.com>
2022-08-08 15:17:19 -0500

    Initial Commit

---
+++ RandomNumCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomNumCalc
+//
+//  Created by Millan Bhandari on 8/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomNumCalc/RandomNumCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumCalcApp.swift
+//  RandomNumCalc
+//
+//  Created by Millan Bhandari on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
